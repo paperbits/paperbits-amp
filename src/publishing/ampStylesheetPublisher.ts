53827447d7a94ee89cdd67aa82c86828b000f72e
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-02-08 23:28:16 -0800

    tmp

--- src/publishing/ampStylesheetPublisher.ts
+++ src/publishing/ampStylesheetPublisher.ts
@@ -17,7 +17,7 @@ export class AmpStylesheetPublisherPlugin implements HtmlPagePublisherPlugin {
         const styleSheets = styleManager.getAllStyleSheets();
         const compiler = new JssCompiler();
 
-        let css = "*{position:relative}body,html{height:100%;width:100%;display:flex;flex-direction:column}.stretch{flex:1;height:100%}layout,main{flex:1;flex-direction:column;display:flex}.block{display:block;flex-basis:100%} html:not([amp4ads]),html:not([amp4ads]) body {height: 100% !important;}";
+        let css = "*{position:relative}body,html{height:100%;width:100%;display:flex;flex-direction:column}.stretch{flex:1;height:100%}layout,main{flex:1;flex-direction:column;display:flex}.block{display:block;flex-basis:100%} html:not([amp4ads]),html:not([amp4ads]) body{min-height:100%;display:flex;flex:1;}";
         styleSheets.forEach(styleSheet => {
             css += " " + compiler.styleSheetToCss(styleSheet);
         });
