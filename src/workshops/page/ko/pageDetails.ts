e437bcefe1844f410fcbfaf9e4cf7aa61100501d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-03-07 16:30:12 -0800

    Minor fix for social share image selector.

--- src/workshops/page/ko/pageDetails.ts
+++ src/workshops/page/ko/pageDetails.ts
@@ -134,7 +134,7 @@ export class PageDetailsWorkshop {
             this.socialShareImage(null);
         }
 
-        const socialShareData = this.pageItem.socialShareData();
+        const socialShareData = this.pageItem.socialShareData() || {};
 
         socialShareData.image = {
             sourceKey: media.key,
