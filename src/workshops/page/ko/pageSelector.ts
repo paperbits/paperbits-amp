b20e9163167d22d012bfa51a5f0a84fa4fdd001e
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-04-19 09:50:20 -0700

    Improvements to hyperlink selector and related components.

--- src/workshops/page/ko/pageSelector.ts
+++ src/workshops/page/ko/pageSelector.ts
@@ -2,7 +2,7 @@ import * as ko from "knockout";
 import template from "./pageSelector.html";
 import { IResourceSelector } from "@paperbits/common/ui";
 import { PageItem, AnchorItem } from "./pageItem";
-import { IPageService } from "@paperbits/common/pages";
+import { IPageService, PageContract } from "@paperbits/common/pages";
 import { Component, Param, Event, OnMounted } from "@paperbits/common/ko/decorators";
 import { HyperlinkModel } from "@paperbits/common/permalinks";
 import { AnchorUtils } from "@paperbits/core/text/anchorUtils";
@@ -13,24 +13,28 @@ import { ChangeRateLimit } from "@paperbits/common/ko/consts";
     template: template
 })
 export class PageSelector implements IResourceSelector<HyperlinkModel> {
+    private preSelectedModel: HyperlinkModel;
+
     public readonly searchPattern: ko.Observable<string>;
     public readonly pages: ko.ObservableArray<PageItem>;
     public readonly working: ko.Observable<boolean>;
 
-    private preSelectedModel: HyperlinkModel;
+    constructor(private readonly ampPageService: IPageService) {
+        this.pages = ko.observableArray();
+        this.selectedPage = ko.observable();
+        this.searchPattern = ko.observable("");
+        this.working = ko.observable();
+    }
 
     @Param()
     public selectedPage: ko.Observable<PageItem>;
 
     @Event()
-    public onSelect: (selection: HyperlinkModel) => void;
+    public onSelect: (selection: PageContract) => void;
 
-    constructor(private readonly ampPageService: IPageService) {
-        this.pages = ko.observableArray();
-        this.selectedPage = ko.observable();
-        this.searchPattern = ko.observable();
-        this.working = ko.observable();
-    }
+    @Event()
+    public onHyperlinkSelect: (selection: HyperlinkModel) => void;
+
 
     @OnMounted()
     public async onMounted(): Promise<void> {
@@ -84,10 +88,22 @@ export class PageSelector implements IResourceSelector<HyperlinkModel> {
         this.selectedPage(page);
         page.isSelected(true);
 
-        await this.getAnchors(page);
+        if (!page.anchorsLoaded()) { // expand anchors on first click
+            const anchors = await this.getAnchors(page);
+            page.anchors(anchors);
+            page.anchorsLoaded(true);
+
+            if (anchors.length > 0) {
+                return;
+            }
+        }
 
         if (this.onSelect) {
-            this.onSelect(page.getHyperlink());
+            this.onSelect(page.toContract());
+        }
+
+        if (this.onHyperlinkSelect) {
+            this.onHyperlinkSelect(page.getHyperlink());
         }
     }
 
@@ -96,33 +112,31 @@ export class PageSelector implements IResourceSelector<HyperlinkModel> {
     }
 
     public async selectAnchor(anchor: AnchorItem): Promise<void> {
-        if (this.onSelect) {
-            const selectedPage = this.selectedPage();
-            anchor.isSelected(true);
-            selectedPage.selectedAnchor = anchor;
-            this.onSelect(selectedPage.getHyperlink());
+        if (!this.onHyperlinkSelect) {
+            return;
         }
+
+        const selectedPage = this.selectedPage();
+        anchor.isSelected(true);
+        selectedPage.selectedAnchor = anchor;
+
+        this.onHyperlinkSelect(selectedPage.getHyperlink());
     }
 
-    private async getAnchors(pageItem: PageItem): Promise<void> {
+    private async getAnchors(pageItem: PageItem): Promise<AnchorItem[]> {
         const pageContent = await this.ampPageService.getPageContent(pageItem.key);
         const children = AnchorUtils.getHeadingNodes(pageContent, 1, 6);
-        let selectedAnchor: AnchorItem;
 
-        const anchors = children.map(item => {
-            const anchor = new AnchorItem();
-            anchor.shortTitle = item.nodes[0].text;
-            anchor.elementId = item.attrs.id;
-            if (pageItem.selectedAnchor && pageItem.selectedAnchor.elementId === anchor.elementId) {
-                selectedAnchor = anchor;
-            }
-            return anchor;
-        });
+        const anchors = children
+            .filter(item => item.nodes?.length > 0)
+            .map(item => {
+                const anchor = new AnchorItem();
+                anchor.shortTitle = item.nodes[0]?.text;
+                anchor.elementId = item.attrs.id;
 
-        pageItem.anchors(anchors);
+                return anchor;
+            });
 
-        if (selectedAnchor) {
-            this.selectAnchor(selectedAnchor);
-        }
+        return anchors;
     }
 }
