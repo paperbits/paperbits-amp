540c8a1307f63d470a29a7a922ff0e733225d3d2
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-10-21 16:20:56 -0700

    Improved control state style management.

--- src/widgets/picture/picture.design.module.ts
+++ src/widgets/picture/picture.design.module.ts
@@ -13,7 +13,6 @@ export class PictureDesignModule implements IInjectorModule {
             key: "picture",
             name: "components_picture", 
             groupName: "Pictures",
-            selectorTemplate: `<img src="https://cdn.paperbits.io/images/placeholder-340x190.jpg" alt="Picture" data-bind="css: classNames" width="280px" height="190px" /><div data-bind="text: displayName"></div>`,
             styleTemplate: `<img src="https://cdn.paperbits.io/images/placeholder-340x190.jpg" alt="Picture" data-bind="stylePreview: variation" width="340px" height="190px" />`
         };
 
